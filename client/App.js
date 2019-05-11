60e72efef68c3d14cbe268468918deda0a3b46bf
mrkwon <erased11@gmail.com>
2019-05-12 01:20:31 +0900

    [추가] ios SplashScreen 구현

--- client/App.js
+++ client/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,6 +10,9 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide()
+  }
   render() {
     return (
       <View style={styles.container}>
